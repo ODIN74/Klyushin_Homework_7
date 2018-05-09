92dd0f82625dbe1f6f17bf984b69793e4a9e96bd
ODIN74 <fantompus@gmail.com>
2018-05-09 10:12:34 +0500

    Реализовано 2 задание

---
+++ Task_2/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Task_2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStartGame = new System.Windows.Forms.Button();
+            this.tbEnteredNumber = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnCompare = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStartGame
+            // 
+            this.btnStartGame.Location = new System.Drawing.Point(94, 174);
+            this.btnStartGame.Name = "btnStartGame";
+            this.btnStartGame.Size = new System.Drawing.Size(87, 23);
+            this.btnStartGame.TabIndex = 0;
+            this.btnStartGame.Text = "Начать игру";
+            this.btnStartGame.UseVisualStyleBackColor = true;
+            this.btnStartGame.Click += new System.EventHandler(this.btnStartGame_Click);
+            // 
+            // tbEnteredNumber
+            // 
+            this.tbEnteredNumber.Enabled = false;
+            this.tbEnteredNumber.Location = new System.Drawing.Point(94, 40);
+            this.tbEnteredNumber.MaxLength = 3;
+            this.tbEnteredNumber.Name = "tbEnteredNumber";
+            this.tbEnteredNumber.Size = new System.Drawing.Size(54, 20);
+            this.tbEnteredNumber.TabIndex = 1;
+            this.tbEnteredNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.tbEnteredNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbEnteredNumber_KeyPress);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.Color.Blue;
+            this.lblResult.Location = new System.Drawing.Point(26, 105);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 19);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.Enabled = false;
+            this.btnCompare.Location = new System.Drawing.Point(166, 38);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(100, 23);
+            this.btnCompare.TabIndex = 3;
+            this.btnCompare.Text = "Сравнить";
+            this.btnCompare.UseVisualStyleBackColor = true;
+            this.btnCompare.Click += new System.EventHandler(this.btnCompare_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ваше число:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(289, 216);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCompare);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.tbEnteredNumber);
+            this.Controls.Add(this.btnStartGame);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Угадай число";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartGame;
+        private System.Windows.Forms.TextBox tbEnteredNumber;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnCompare;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Task_2/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_2
+{
+    public partial class MainForm : Form
+    {
+        Random rnd = new Random();
+        int hiddenNumber = 0;
+        int counter = 0;
+        
+        public MainForm()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnStartGame_Click(object sender, EventArgs e)
+        {
+            hiddenNumber = rnd.Next(1, 101);
+            btnStartGame.Enabled = false;
+            btnCompare.Enabled = true;
+            tbEnteredNumber.Enabled = true;
+            lblResult.Text = String.Empty;
+            lblResult.ForeColor = Color.Blue;
+        }
+
+        private void tbEnteredNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsDigit(e.KeyChar) || (int)e.KeyChar == (int)Keys.Back || (int)e.KeyChar == (int)Keys.Enter))
+            {
+                e.Handled = true;
+            }
+            else if((int)e.KeyChar != (int)Keys.Enter && (int)e.KeyChar != (int)Keys.Back && int.Parse(tbEnteredNumber.Text.Insert(tbEnteredNumber.Text.Length, e.KeyChar.ToString())) > 100)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnCompare_Click(object sender, EventArgs e)
+        {
+            if (hiddenNumber == int.Parse(tbEnteredNumber.Text))
+            {
+                counter++;
+                lblResult.ForeColor = Color.ForestGreen;
+                lblResult.Text = $"Вы угадали с {counter} попытки.\nЭто действительно {hiddenNumber}.";
+                btnStartGame.Enabled = true;
+                btnCompare.Enabled = false;
+                tbEnteredNumber.Enabled = false;
+            }
+            else if (hiddenNumber > int.Parse(tbEnteredNumber.Text))
+            {
+                lblResult.Text = "Загаданное число больше";
+                counter++;
+            }
+            else
+            {
+                lblResult.Text = "Загаданное число меньше";
+                counter++;
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.timer1.Stop();
+            MessageBox.Show(this, "В данной программе вам необходимо угадать\nчисло от 1 до 100, загаданное компьютером.", "О программе");    
+        }
+    }
+}
